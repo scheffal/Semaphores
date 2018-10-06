479c9bd79597db5f3305bc11716be2c5d9e8e1a9
scheffal <scheffal@mail.gvsu.edu>
2018-10-06 12:05:41 -0400

    Cleaned up

--- semaphore.c
+++ semaphore.c
@@ -1,3 +1,12 @@
+/***************************
+ * Lab 6 - Semaphores
+ * CIS 452-10
+ * 10/11/18
+ *
+ * Authors:
+ * Ali Scheffler
+ * Anthony Blanton
+ * ************************/
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <unistd.h> 
@@ -29,11 +38,13 @@ int main (int arc, char *argv[])
       exit (1); 
    }
 
-   //Create a new semaphore eand initialize semaphore
+   //Create a new semaphore and initialize semaphore
    int semId = semget(IPC_PRIVATE, 1, 00600);
    semctl(semId, 0, SETVAL, 1);
 
+   //Create structure
    struct sembuf sbuf;
+
    int result;
    shmPtr[0] = 0; 
    shmPtr[1] = 1;
@@ -44,16 +55,18 @@ int main (int arc, char *argv[])
 
    if (!(pid = fork())) { 
       for (i=0; i<loop; i++) {
+	        //lock
 	 	sbuf.sem_op = -1;   
-
-                result = semop(semId, &sbuf, 1); 
-	      // swap the contents of shmPtr[0] and shmPtr[1]
+                result += semop(semId, &sbuf, 1); 
+	        
+		// swap the contents of shmPtr[0] and shmPtr[1]
 		temp = shmPtr[0];
 		shmPtr[0] = shmPtr[1];
 		shmPtr[1] = temp;
 
+		//Unlock
 		sbuf.sem_op = 1;
-		result = semop(semId, &sbuf, 1);
+		result += semop(semId, &sbuf, 1);
       } 
       if (shmdt (shmPtr) < 0) { 
          perror ("just can't let go\n"); 
@@ -63,15 +76,24 @@ int main (int arc, char *argv[])
    } 
    else 
       for (i=0; i<loop; i++) { 
-	        sbuf.sem_op = -1;
-		result = semop(semId, &sbuf, 1);
-               // swap the contents of shmPtr[1] and shmPtr[0] 
+	        //lock
+	      	sbuf.sem_op = -1;
+		result += semop(semId, &sbuf, 1);
+                
+		// swap the contents of shmPtr[1] and shmPtr[0] 
       		temp = shmPtr[0];
 		shmPtr[0] = shmPtr[1];
 		shmPtr[1] = temp;
+
+		//unlock
 		sbuf.sem_op = 1;
-		result = semop(semId, &sbuf, 1);
+		result += semop(semId, &sbuf, 1);
       }
+
+   if(result < 0)
+   {
+	   printf("Unsuccesful\n");
+   }
 
    wait (&status); 
    printf ("values: %li\t%li\n", shmPtr[0], shmPtr[1]);
